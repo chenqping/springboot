eab4239a49abae94890f7d630385f1358965f200
cqping <cqping@qq.com>
2019-02-19 16:28:51 +0800

    add log Hello version

--- bookpub/src/main/java/org/test/bookpub/BookPubApplication.java
+++ bookpub/src/main/java/org/test/bookpub/BookPubApplication.java
@@ -2,6 +2,34 @@ package org.test.bookpub;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+/*
+* @SpringBootApplicaton annotation:
+* @Configuration
+* @EnableAutoConfiguration
+* @ComponentScan
+@Configuration tells Spring (and not just Spring Boot, as it is a Spring Framework
+core annotation) that the annotated class contains Spring configuration definitions
+such as the @Bean, @Component, and @Service declarations, and others.
+@ComponentScan tells Spring that we want to scan our application packages—starting
+from the package of our annotated class as a default package root—for the other
+classes that might be annotated with @Configuration, @Controller, and other
+applicable annotations, which Spring will automatically include as part of the context
+configuration.
+@EnableAutoConfiguration is a part of the Spring Boot annotation, which is a meta-annotation
+on its own (you will find that Spring libraries rely very heavily on the
+meta-annotations in order to group and compose configurations together).It imports
+the EnableAutoConfigurationImportSelector and
+AutoConfigurationPackages.Registrar classes that effectively instruct Spring to
+automatically configure the conditional beans depending on the classes available in
+the classpath.
+* */
+
+/*
+* To run the application:
+* mvn spring-boot:run
+ * */
 
 @SpringBootApplication
 public class BookPubApplication {
@@ -10,4 +38,8 @@ public class BookPubApplication {
 		SpringApplication.run(BookPubApplication.class, args);
 	}
 
+	@Bean
+	public StartupRunner schedulerRunner(){
+		return new StartupRunner();
+	}
 }
---
+++ bookpub/src/main/java/org/test/bookpub/StartupRunner.java
@@ -0,0 +1,13 @@
+package org.test.bookpub;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.boot.CommandLineRunner;
+
+public class StartupRunner implements CommandLineRunner {
+    protected final Log logger = LogFactory.getLog(getClass());
+    @Override
+    public void run(String... args){
+        logger.info("Hello");
+    }
+}
