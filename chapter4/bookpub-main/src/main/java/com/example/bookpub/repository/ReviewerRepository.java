f4fa7192ca1d8bba8d4f56197be7ce75a4b65aa3
cqping <cqping@qq.com>
2019-03-08 12:06:39 +0800

    finish chapter 4 creating a custom Spring Boot autoconfiguration starter

---
+++ chapter4/bookpub-main/src/main/java/com/example/bookpub/repository/ReviewerRepository.java
@@ -0,0 +1,7 @@
+package com.example.bookpub.repository;
+
+import com.example.bookpub.entity.Reviewer;
+import org.springframework.data.repository.PagingAndSortingRepository;
+
+public interface ReviewerRepository extends PagingAndSortingRepository<Reviewer, Long> {
+}
