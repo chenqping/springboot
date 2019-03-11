7696dbb4ed4f074c7261aa59b6691c9a1faed72a
cqping <cqping@qq.com>
2019-03-11 15:06:57 +0800

    add missed annotation @Repository to BookRepository

--- bookpub/src/main/java/org/example/bookpub/repository/BookRePository.java
+++ bookpub/src/main/java/org/example/bookpub/repository/BookRePository.java
@@ -2,7 +2,9 @@ package org.example.bookpub.repository;
 
 import org.example.bookpub.entity.Book;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface BookRePository extends CrudRepository<Book, Long> {
     public Book findBookByIsbn(String isbn);
 }
