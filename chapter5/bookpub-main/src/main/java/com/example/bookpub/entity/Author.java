58ade491e68e4cf7d7dba16ab4412356549ef455
cqping <cqping@qq.com>
2019-03-12 17:19:29 +0800

    chapter 5 Creating a JPA Component test

--- chapter5/bookpub-main/src/main/java/com/example/bookpub/entity/Author.java
+++ chapter5/bookpub-main/src/main/java/com/example/bookpub/entity/Author.java
@@ -22,4 +22,20 @@ public class Author {
         this.firstName = firstName;
         this.lastName = lastName;
     }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
 }
