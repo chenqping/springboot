da33e557e7e4567b846e4a451173fc357b9cc5a6
cqping <cqping@qq.com>
2019-03-13 14:41:48 +0800

    add chapter 6

---
+++ chapter6/bookpub-main/src/main/java/com/example/bookpub/entity/Book.java
@@ -0,0 +1,66 @@
+package com.example.bookpub.entity;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+public class Book {
+    @Id
+    @GeneratedValue
+    private Long id;
+    private String isbn;
+    private String title;
+    private String description;
+
+    @ManyToOne
+    private Author author;
+    @ManyToOne
+    private Publisher publisher;
+    @ManyToMany
+    private List<Reviewer> reviewers;
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public List<Reviewer> getReviewers() {
+        return reviewers;
+    }
+
+    public void setReviewers(List<Reviewer> reviewers) {
+        this.reviewers = reviewers;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Publisher getPublisher() {
+        return publisher;
+    }
+
+    protected Book() {}
+
+    public Book(String isbn, String title, Author author, Publisher publisher){
+        this.isbn = isbn;
+        this.title = title;
+        this.author = author;
+        this.publisher = publisher;
+    }
+}
---
+++ chapter6/bookpub-main/src/main/java/com/example/bookpub/repository/AuthorRepository.java
@@ -0,0 +1,15 @@
+package com.example.bookpub.repository;
+
+import com.example.bookpub.entity.Author;
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+/*
+The @RepositoryRestResource annotation, while optional, provides us with the ability to have finer control
+over the exposure of the repository as a web data service. For example, if we wanted to change the
+URL path or rel value, to writers instead of authors, we could have tuned the annotation as follows:
+@RepositoryRestResource(collectionResourceRel = "writers", path = "writers")
+* */
+@RepositoryRestResource
+public interface AuthorRepository extends PagingAndSortingRepository<Author, Long> {
+}
